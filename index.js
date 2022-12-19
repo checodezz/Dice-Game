58a55304d8828d8c77c6e99c2293d220e3d89e13
chethanronaldo <53275990+chethanronaldo@users.noreply.github.com>
2022-12-19 12:34:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //to generate random number betweeen 1-6
+
+var randomDiceImage = "dice" + randomNumber1 +".png"; //dice.1 png - dice6.png
+
+var randomImageSource = "images/" +randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//if player1 wins
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "# Player 1 Wins🎉";
+} 
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "# Player 2 Wins🎉";
+}
+else {
+    document.querySelector("h1").innerHTML = "DRAW!☯";
+}
